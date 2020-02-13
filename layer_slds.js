956ae55c5b5c61f9b2256bd13b85eaa38785554c
anneb <anne.blankert@geodan.nl>
2020-02-13 12:41:36 +0100

    make param 'table' for get /data/slds optional

--- layer_slds.js
+++ layer_slds.js
@@ -8,79 +8,65 @@ where (c.relkind = ANY (ARRAY['r'::"char", 'v'::"char", 'm'::"char", 'f'::"char"
 and a.attname='dblayer'
 and t.typname='varchar'
 AND NOT pg_is_other_temp_schema(c.relnamespace) 
-AND has_table_privilege(c.oid, 'SELECT'::text)`
+AND has_table_privilege(c.oid, 'SELECT'::text)`;
+
 
 module.exports = function (app, pool) {
 /**
  * @swagger
  *
- * /data/layer_slds/:table:
+ * /data/slds/:table:
  *   get:
- *     description: Returns a list sldtables referencing the passed in table name.
+ *     description: Returns a list sldtables.
  *     tags: ['meta']
  *     summary: 'list sld tables'
  *     produces:
  *       - application/json
  *     parameters:
  *       - name: table
- *         description: The name of the table to find sld tables for
+ *         description: Optional name of the table to filter sld tables for
  *         in: path
- *         required: true
+ *         required: false
  *         type: string 
  *     responses:
  *       200:
- *         description: json array of sldlayers referencing the given table [{sldtable: string, sldlayer: string, dblayer: string}]
+ *         description: json array of sldlayers [{sldtable: string, sldlayer: string, dblayer: string}]
  */
-  app.get('/data/layer_slds/:table', async (req, res)=> {
-    let tableName, schemaName;
-    const table = req.params.table;
-    if (table) {
-      const parts = table.split('.');
-      if (parts.length === 1) {
-        tableName = parts[0];
-        schemaName = null;
-      } else {
-        schemaName = parts[0];
-        tableName = parts[1];
+  app.get('/data/slds/:table?', async (req, res)=> {
+    let table = req.params.table;
+    let parts;
+    try {
+      // get sld tables in database
+      let sldtables = await pool.query(getAllSLDTablesSql);
+      let result = [];        
+      for (let i = 0; i < sldtables.length; i++) {
+        let sql = 'select distinct sldlayer, dblayer from $(namespace:name).$(tablename:name)';
+        let sqlParams = {namespace: sldtables[i].namespace, tablename: sldtables[i].tablename};
+        let layers = await pool.any(sql, sqlParams);
+        result = result
+          .concat(
+            layers
+              .filter(layer=>table?layer.dblayer===table:true)
+              .map(layer=>{
+                return {sldtable: `${sldtables[i].namespace}.${sldtables[i].tablename}`, sldlayer: layer.sldlayer, dblayer: layer.dblayer}
+              }));
       }
-      req.params.table = tableName;
-      req.params.schema = schemaName;
-      const sqlString = sql(req.params, req.query);
-      try {
-        // get sld tables in database
-        let sldtables = await pool.query(getAllSLDTablesSql);
-        let result = [];        
-        for (let i = 0; i < sldtables.length; i++) {
-          let sql = 'select distinct sldlayer, dblayer from $(namespace:name).$(tablename:name)';
-          let sqlParams = {namespace: sldtables[i].namespace, tablename: sldtables[i].tablename};
-          let layers = await pool.any(sql, sqlParams);
-          result = result
-            .concat(
-              layers
-                .filter(layer=>layer.dblayer===table)
-                .map(layer=>{
-                  return {sldtable: `${sldtables[i].namespace}.${sldtables[i].tablename}`, sldlayer: layer.sldlayer, dblayer: layer.dblayer}
-                }));
-        }
-        res.json(result);
-      } catch (err) {
-        console.log(err);
-        let status = 500;
-        switch (err.code) {
-            case '42P01':
-                // table does not exist
-                status = 422;
-                break;
-            case '42703':
-                // column does not exist
-                status = 422;
-                break;
-            default:
-        }
-        res.status(status).json({error:err.message})
+      res.json(result);
+    } catch (err) {
+      console.log(err);
+      let status = 500;
+      switch (err.code) {
+          case '42P01':
+              // table does not exist
+              status = 422;
+              break;
+          case '42703':
+              // column does not exist
+              status = 422;
+              break;
+          default:
       }
-    } else {
-      res.status(422).json({error:"missing parameter 'table'"})
+      res.status(status).json({error:err.message})
     }
   });
 }
